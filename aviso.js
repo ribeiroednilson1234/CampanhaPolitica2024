f6e8ff42734ddaaee2ec50f657e3b3b02d85652e
Ednilson Alves <130374794+ribeiroednilson1234@users.noreply.github.com>
2024-06-19 10:28:45 -0300

    Update aviso.js

--- aviso.js
+++ aviso.js
@@ -1,7 +1,6 @@
 <script>
-window.postMessage();
-alert="Aviso"
-{
-    Aviso("Você está prestes a escolher o seu candidato./n Faça a    escolha consciente.");
+window.postMessage.alert();
+{let alert="Aviso";
+    Aviso("Você está prestes a escolher o seu candidato./n Faça a escolha consciente.");
 };
 </script>
