47ee76831046a1879a1583e88da74d124759b007
Ednilson Alves <130374794+ribeiroednilson1234@users.noreply.github.com>
2024-06-22 21:13:09 -0300

    Add files via upload
    Signed-off-by: Ednilson Alves <130374794+ribeiroednilson1234@users.noreply.github.com>

--- aviso.js
+++ aviso.js
@@ -1,6 +1,6 @@
 <script>
 window.postMessage.alert();
-{let alert="Aviso";
+{let alert=Aviso;
     Aviso("Você está prestes a escolher o seu candidato./n Faça a escolha consciente.");
-};
+}
 </script>
