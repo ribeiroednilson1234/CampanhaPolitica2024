348efe64a41f0763d07f5a57b9a32d5fed2e7251
Ednilson Alves <130374794+ribeiroednilson1234@users.noreply.github.com>
2024-06-24 19:34:39 -0300

    Add files via upload
    Signed-off-by: Ednilson Alves <130374794+ribeiroednilson1234@users.noreply.github.com>

--- aviso.js
+++ aviso.js
@@ -1,12 +1,8 @@
 <script>
-window.addEventListener(
-  "message",
-  (event) => {
-    if (event.source !== alert("Você está prestes a escolher o seu candidato. Faça a escolha consciente.")) return;
-
-    // …
-  },
-  false,
-)
+    window.addEventListener("message", (event) => {
+        if ( event.source.postMessage(
+            "Você está prestes a escolher o seu candidato. Faça a escolha consciente.",
+    event.origin,
+  ));
+});
 </script>
-
