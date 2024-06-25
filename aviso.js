716294b29f07bd11f7df920947d7d7a86bb87d7d
Ednilson Alves <130374794+ribeiroednilson1234@users.noreply.github.com>
2024-06-25 15:30:42 -0300

    Update aviso.js
    Signed-off-by: Ednilson Alves <130374794+ribeiroednilson1234@users.noreply.github.com>

--- aviso.js
+++ aviso.js
@@ -1,8 +1,7 @@
 <script>
     window.addEventListener("message", (event) => {
-        if ( event.source.postMessage(
-            "Você está prestes a escolher o seu candidato. Faça a escolha consciente.",
+        if ( event.source.postMessage(("Você está prestes a escolher o seu candidato. Faça a escolha consciente."),
     event.origin,
-  ));
-});
+    );
+} true);
 </script>
